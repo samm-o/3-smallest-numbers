843466f14ebfec88f57d93d980c4dbbadd23d3c6
samm-o <samuilsgeorgiev@gmail.com>
2023-01-30 10:11:55 -0500

    Add project files.

---
+++ 3 smallest numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3_smallest_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a series of 5 numbers: ");
+            var input = Console.ReadLine();
+            var numbers = Array.ConvertAll(input.Split(','), int.Parse).ToList();
+
+            if (numbers.Count < 5)
+            {
+                Console.WriteLine("Invalid list");
+            }
+            else
+            {
+                numbers.Sort();
+                Console.WriteLine(numbers[0]);
+                Console.WriteLine(numbers[1]);
+                Console.WriteLine(numbers[2]);
+            }
+            
+        }
+    }
+}
